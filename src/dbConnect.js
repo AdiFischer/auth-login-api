a0172a9fbbcfb92a8d89e444bd9d6201df56c874
AdiFischer <adifischer1@gmail.com>
2022-11-05 11:13:15 -0400

    api for login react project

---
+++ src/dbConnect.js
@@ -0,0 +1,12 @@
+import { initializeApp, cert ,getApps } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore"
+import secrets from "../secrets.js";
+
+export default function dbConnect() {
+    if(!getApps().length) {  //r we connected? not-empty[]we wanna connect return the connection to fistore
+        initializeApp({
+            credential: cert(secrets)
+        })
+    }
+    return getFirestore()
+}
